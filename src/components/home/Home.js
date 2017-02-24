d9590c2cf2ac523031dec06706ddf6adec76bdf6
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-24 16:10:52 -0600

    rendering home component with landing page text

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react'
 import NavBar from '../navBar'
 
-class Home extends Component{
+class Home extends Component {
     render() {
         return (
             <div className = 'home'>
                 <p>This is the home component</p>
                 <NavBar />
+                <HomeComponent />
+            </div>
+        )
+    }
+}
+
+class HomeComponent extends Home {
+    render() {
+        return (
+            <div className = 'homeComponent'>
+                <h1>Welcome to Live Draw</h1>
+                <h2>Paint along with other users!</h2>
             </div>
         )
     }
