81123b7293cce262993f3262a25d78e0e08f5246
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-24 16:19:26 -0600

    login component rendering sign in and login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,8 +5,36 @@ class Login extends Component {
     render() {
         return (
             <div className = 'login'>
-                <p>This is the login component</p>
                 <NavBar />
+                <LoginComponent />
+            </div>
+        )
+    }
+}
+
+class LoginComponent extends Login {
+    render() {
+        return (
+            <div className = 'login'>
+                <div className = 'signin'>
+                    <h3>Sign Up</h3>
+                    <div className = 'loginInputs'>
+                        <input type = 'email' placeholder = 'email'/>
+                        <input type = 'email' placeholder = 'confirm email'/>
+                        <input type = 'password' placeholder = 'password'/>
+                        <input type = 'password' placeholder = 'confirm password'/>
+                        <input type = 'submit' name = 'signUpSubmit'/>
+                    </div>
+                </div>
+
+                <div className = 'signin'>
+                    <h3>Log In</h3>
+                    <div className = 'loginInputs'>
+                        <input type = 'email' placeholder = 'email'/>
+                        <input type = 'password' placeholder = 'password'/>
+                        <input type = 'submit' name = 'logInSubmit'/>
+                    </div>
+                </div>
             </div>
         )
     }
