bbeeb886b047798b0cfd536addc8992ce3289c6d
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-24 15:13:33 -0600

    created routes object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Backbone from 'backbone'
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+
+const Controller = Backbone.Router.extend({
+    routes: {
+        'home': '_handleHome',
+        'canvas': '_handleCanvas',
+        'login': '_handleLogin',
+        '*default': '_handleDeafulat'
+    },
+})
 
 ReactDOM.render(
   <App />,
