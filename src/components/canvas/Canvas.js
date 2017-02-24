dbc11c7f37023f63963ac252989fc51983b9adfa
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-24 16:02:52 -0600

    added navbar to each component and links changing hash and rendering correct view

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import NavBar from '../navBar'
 
 class Canvas extends Component {
     render() {
         return (
             <div className = 'canvas'>
                 <p>This is the canvas component</p>
+                <NavBar />
             </div>
         )
     }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import NavBar from '../navBar'
 
 class Login extends Component {
     render() {
         return (
             <div className = 'login'>
                 <p>This is the login component</p>
+                <NavBar />
             </div>
         )
     }
