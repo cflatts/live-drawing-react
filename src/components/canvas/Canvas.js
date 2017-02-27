3f6ea693139ba2f4db798f4676ab5fab435555b3
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-27 12:34:26 -0600

    added shape divs and canvas element to canvas component

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -24,6 +24,17 @@ class CanvasComponent extends Canvas {
                     <div className='color blue' name='blue'></div>
                     <div className='color purple' name='purple'></div>
                 </div>
+
+                <div className='drawingShapes'>
+                    <div className='shape line' name='line'>Line</div>
+                    <div className='shape circle' name='circle'>Circ</div>
+                    <div className='shape rectangle' name='rectangle'>Rect</div>
+                    <div className='shape triangle' name='triangle'>Tri</div>
+                    <div className='shape freestyle' name='freestyle'>Free</div>
+                    <div className='shape erase' name='erase'>Erase</div>
+                </div>
+
+                <canvas id='myCanvas' width='1200' height='600'></canvas>
             </div>
         )
     }
