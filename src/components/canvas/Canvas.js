1d91ec711646b021d233781bd92cbf9059a83105
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-27 12:20:01 -0600

    created color divs for drawing

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react'
 import NavBar from '../navBar'
 
 class Canvas extends Component {
-
     render() {
         return (
             <div className='canvas'>
-                <p>This is the canvas component</p>
                 <NavBar />
+                <CanvasComponent />
+            </div>
+        )
+    }
+}
+
+class CanvasComponent extends Canvas {
+    render() {
+        return (
+            <div className='drawingArea'>
+                <div className ='drawingColors'>
+                    <div className='color red' name='red'></div>
+                    <div className='color orange' name='orange'></div>
+                    <div className='color yellow' name='yellow'></div>
+                    <div className='color green' name='green'></div>
+                    <div className='color blue' name='blue'></div>
+                    <div className='color purple' name='purple'></div>
+                </div>
             </div>
         )
     }
