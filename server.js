86ed493d7807445c4405f911a79cba6d6f124c3d
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-28 14:50:32 -0600

    set up dev server to proxy to express server on port 3001

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
 
-
+//Static files served out of build file
 app.use(express.static('build'))
 
+//Production server listening on port 3001
 app.listen(3001, function() {
     console.log('App is listening on port 3001')
 })
