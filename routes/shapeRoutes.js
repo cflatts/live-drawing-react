aca5d1efaea7129840c815e1fa94f75832ba393b
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-01 13:56:27 -0600

    installed body-parser dependency

--- routes/shapeRoutes.js
+++ routes/shapeRoutes.js
@@ -1,2 +1,10 @@
 let Router = require('express').Router
 let apiRouter = Router()
+
+let ShapeModel = require('./db/schema.js').Shape
+
+//Return all the shapes
+
+apiRouter.get('/shapes', function(req, res) {
+
+})
