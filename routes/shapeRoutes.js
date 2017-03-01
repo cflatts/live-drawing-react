341ea45afd0e71cfc784ca80cd74e8a3e0bdf015
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-01 14:00:20 -0600

    imported mongoose shape object to shaperoutes, created bodyparser middleware in server

--- routes/shapeRoutes.js
+++ routes/shapeRoutes.js
@@ -2,9 +2,3 @@ let Router = require('express').Router
 let apiRouter = Router()
 
 let ShapeModel = require('./db/schema.js').Shape
-
-//Return all the shapes
-
-apiRouter.get('/shapes', function(req, res) {
-
-})
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 //Static files served out of build file
 app.use(express.static('build'))
+
+//App middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 //Production server listening on port 3001
 app.listen(3001, function() {
