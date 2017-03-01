26406b687300919cfa291266361ddb820f5a3509
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-28 18:01:56 -0600

    exported model and collection

--- src/models/shapeModels.js
+++ src/models/shapeModels.js
@@ -1,11 +1,13 @@
 import Backbone from 'backbone'
 import $ from 'jquery'
 
-var ShapeModel = Backbone.Model.extend({
+export const ShapeModel = Backbone.Model.extend({
     idAttribute: '_id'
 })
 
-var ShapeCollection = Backbone.Collection.extend({
+export const ShapeCollection = Backbone.Collection.extend({
     url: '/api/shapes',
     model: ShapeModel
 })
+
+export {ShapeModel, ShapeCollection}
