ba341d0261620f188baac7d9e03ad489cb58675c
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-01 19:22:47 -0600

    created switch case for adding shapes

--- src/data/shapeStore.js
+++ src/data/shapeStore.js
@@ -2,3 +2,23 @@ import Immutable from 'immutable'
 import {ReduceStore} from 'flux/utils'
 import ShapeActionTypes from './shapeActionTypes'
 import ShapeDispatcher from './shapeDispatcher'
+
+class ShapeStore extends ReduceStore {
+    constructor() {
+        super(ShapeDispatcher)
+    }
+
+    getInitialState() {
+        return Immutable.OrderedMap()
+    }
+
+    reduce(state, action) {
+        switch (action.type)) {
+            case ShapeActionTypes.ADD_SHAPE:
+                return state
+                break;
+            default:
+                return state
+        }
+    }
+}
