8cf4fef418717e4f4719f828985fb3afa3f88651
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-01 18:39:03 -0600

    converted actions module to method that dispatches action type

--- src/data/shapeActions.js
+++ src/data/shapeActions.js
@@ -4,15 +4,21 @@ import ShapeActionTypes from './shapeActionTypes'
 
 const ACTIONS = {
 
-    addDot: function(evt) {
-        let myCanvas = evt.target
-        let ctx = myCanvas.getContext('2d')
-        let canvasLocation = myCanvas.getBoundingClientRect()
-
-        const x = evt.clientX - canvasLocation.left
-        const y = evt.clientY - canvasLocation.top
-        ctx.fillRect(x, y, 5, 5)
+    addShape() {
+        ShapeDispatcher.dispatch({
+            type: ShapeActionTypes.ADD_SHAPE
+        })
     }
+
+    // addDot: function(evt) {
+    //     let myCanvas = evt.target
+    //     let ctx = myCanvas.getContext('2d')
+    //     let canvasLocation = myCanvas.getBoundingClientRect()
+    //
+    //     const x = evt.clientX - canvasLocation.left
+    //     const y = evt.clientY - canvasLocation.top
+    //     ctx.fillRect(x, y, 5, 5)
+    // }
 }
 
 export default ACTIONS
