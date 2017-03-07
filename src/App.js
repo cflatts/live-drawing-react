4f3d170ea5d0b7837e795cd18de277a5d3092fb4
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-07 15:21:33 -0600

    imported container components to app

--- src/App.js
+++ src/App.js
@@ -1,2 +1,6 @@
 import React from 'react'
 import {Router, Route, Link} from 'react-router'
+
+import HomeContainer from './components/home/Home'
+import CanvasContainer from './components/canvas/CanvasContainer'
+import LoginContainer from './components/login/Login'
