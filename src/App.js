1fa84399641e678b3969af85811a76b4338ec904
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 15:19:07 -0600

    applied classes to all links in app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
     render() {
         return(
             <div className='appRouter'>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='canvas'>Drawing Canvas</Link></li>
-                    <li><Link to='login'>Login/SignIn</Link></li>
-                </ul>
+                <Link className='link' to='/'>Home</Link>
+                <Link className='link' to='canvas'>Drawing Canvas</Link>
+                <Link className='link' to='login'>Login/SignIn</Link>
                 <div className='homeComponent'>
                     <h1>Welcome to Live Draw</h1>
                     <h2>Paint along with other users!</h2>
