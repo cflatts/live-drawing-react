6a398383fa1701dfb64271c955f4e57c2295ead2
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 14:17:49 -0600

    rendering a react router in index.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import HomeContainer from './components/home/Home'
 import CanvasContainer from './components/canvas/CanvasContainer'
 import LoginContainer from './components/login/Login'
 
-class App extends Component {
-
-    render() {
-        return(
-            <div className='appRouter'>
-                <ul>
-                    <li><Link to='home'>Home</Link></li>
-                    <li><Link to='canvas'>Drawing Canvas</Link></li>
-                    <li><Link to='login'>Login/SignIn</Link></li>
-                </ul>
-                {this.props.children}
-            </div>
-        )
-    }
-}
+// class App extends Component {
+//
+//     render() {
+//         return(
+//             <div className='appRouter'>
+//                 <ul>
+//                     <li><Link to='home'>Home</Link></li>
+//                     <li><Link to='canvas'>Drawing Canvas</Link></li>
+//                     <li><Link to='login'>Login/SignIn</Link></li>
+//                 </ul>
+//                 {this.props.children}
+//             </div>
+//         )
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,17 @@
 import React from 'react'
 // import Backbone from 'backbone'
 import ReactDOM from 'react-dom'
-import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+import {Router, Route, hashHistory} from 'react-router'
 
 import HomeContainer from './components/home/Home'
 import CanvasContainer from './components/canvas/CanvasContainer'
 import LoginContainer from './components/login/Login'
 import './index.css'
 
-// const app = function() {
-//     var Controller = Backbone.Router.extend({
-//         routes: {
-//             'home': '_handleHome',
-//             'canvas': '_handleCanvas',
-//             'login': '_handleLogin',
-//             '*default': '_handleDefault'
-//         },
-//
-//         _handleHome: function() {
-//             ReactDOM.render( <HomeContainer />, document.getElementById('root'))
-//         },
-//
-//         _handleCanvas: function() {
-//             ReactDOM.render(<CanvasContainer/>, document.getElementById('root'))
-//         },
-//
-//         _handleLogin: function() {
-//             ReactDOM.render(<LoginContainer />, document.getElementById('root'))
-//         },
-//
-//         _handleDefault: function() {
-//             location.hash = 'home'
-//         },
-//
-//         initialize: function() {
-//             Backbone.history.start()
-//         }
-//     })
-//     new Controller()
-// }
-// app()
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path='home' component={HomeContainer} />
+        <Route path='canvas' component={CanvasContainer} />
+        <Route path='login' component={LoginContainer} />
+    </Router>
+), document.getElementById('root'))
