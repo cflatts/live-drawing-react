87a177e0f1bd39ce06809da064574025fc2b24b3
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 15:16:20 -0600

    removed home component, now just app component, welcome rendering on each page, want just the nav to render on each page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
 
+import './App.css'
+
 class App extends Component {
 
     render() {
         return(
             <div className='appRouter'>
                 <ul>
-                    <li><Link to='home'>Home</Link></li>
+                    <li><Link to='/'>Home</Link></li>
                     <li><Link to='canvas'>Drawing Canvas</Link></li>
                     <li><Link to='login'>Login/SignIn</Link></li>
                 </ul>
+                <div className='homeComponent'>
+                    <h1>Welcome to Live Draw</h1>
+                    <h2>Paint along with other users!</h2>
+                </div>
                 {this.props.children}
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 
-import HomeContainer from './components/home/Home'
+
 import CanvasContainer from './components/canvas/CanvasContainer'
 import LoginContainer from './components/login/Login'
 import App from './App'
@@ -11,7 +11,6 @@ import './index.css'
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path='/' component={App}>
-            <Route path='/home' component={HomeContainer} />
             <Route path='/canvas' component={CanvasContainer} />
             <Route path='/login' component={LoginContainer} />
         </Route>
