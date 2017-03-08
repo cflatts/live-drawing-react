997b80ffbcd6d87377e2c09a1a5b768eb905f265
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 16:03:49 -0600

    able to render landing text on app.js, removed home component again

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import {Link} from 'react-router'
 import './App.css'
 
 class App extends Component {
-
     render() {
         return(
-            <div className='landingPage'>
-                {/*}<div className='homeComponent'>
-                    <h1>Welcome to Live Draw</h1>
-                    <h2>Paint along with other users!</h2>
-                </div>*/}
-                <div className='appRouter'>
-                    <Link className='link' to='/'>Home</Link>
-                    <Link className='link' to='canvas'>Drawing Canvas</Link>
-                    <Link className='link' to='login'>Login/SignIn</Link>
-                    {this.props.children}
-                </div>
+            <div className='appRouter'>
+                <Link className='link' to='/'>Home</Link>
+                <Link className='link' to='canvas'>Drawing Canvas</Link>
+                <Link className='link' to='login'>Login/SignIn</Link>
+                {this.props.children || (
+                    <div className='homeComponent'>
+                        <h1>Welcome to Live Draw</h1>
+                        <h2>Paint along with other users!</h2>
+                    </div>
+                )}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 
-
 import CanvasContainer from './components/canvas/CanvasContainer'
 import LoginContainer from './components/login/Login'
-import HomeContainer from './components/home.Home'
 import App from './App'
 import './index.css'
 
@@ -15,6 +13,5 @@ ReactDOM.render((
             <Route path='/canvas' component={CanvasContainer} />
             <Route path='/login' component={LoginContainer} />
         </Route>
-
     </Router>
 ), document.getElementById('root'))
