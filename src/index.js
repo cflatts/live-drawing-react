cc4dd802320232504f54399602530c74e7f2aaa0
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 14:07:42 -0600

    added props.children below links so that proper view renders

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 import React from 'react'
-import Backbone from 'backbone'
+// import Backbone from 'backbone'
 import ReactDOM from 'react-dom'
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 
 import HomeContainer from './components/home/Home'
 import CanvasContainer from './components/canvas/CanvasContainer'
 import LoginContainer from './components/login/Login'
 import './index.css'
 
-const app = function() {
-    var Controller = Backbone.Router.extend({
-        routes: {
-            'home': '_handleHome',
-            'canvas': '_handleCanvas',
-            'login': '_handleLogin',
-            '*default': '_handleDefault'
-        },
-
-        _handleHome: function() {
-            ReactDOM.render( <HomeContainer />, document.getElementById('root'))
-        },
-
-        _handleCanvas: function() {
-            ReactDOM.render(<CanvasContainer/>, document.getElementById('root'))
-        },
-
-        _handleLogin: function() {
-            ReactDOM.render(<LoginContainer />, document.getElementById('root'))
-        },
-
-        _handleDefault: function() {
-            location.hash = 'home'
-        },
-
-        initialize: function() {
-            Backbone.history.start()
-        }
-    })
-    new Controller()
-}
-app()
+// const app = function() {
+//     var Controller = Backbone.Router.extend({
+//         routes: {
+//             'home': '_handleHome',
+//             'canvas': '_handleCanvas',
+//             'login': '_handleLogin',
+//             '*default': '_handleDefault'
+//         },
+//
+//         _handleHome: function() {
+//             ReactDOM.render( <HomeContainer />, document.getElementById('root'))
+//         },
+//
+//         _handleCanvas: function() {
+//             ReactDOM.render(<CanvasContainer/>, document.getElementById('root'))
+//         },
+//
+//         _handleLogin: function() {
+//             ReactDOM.render(<LoginContainer />, document.getElementById('root'))
+//         },
+//
+//         _handleDefault: function() {
+//             location.hash = 'home'
+//         },
+//
+//         initialize: function() {
+//             Backbone.history.start()
+//         }
+//     })
+//     new Controller()
+// }
+// app()
